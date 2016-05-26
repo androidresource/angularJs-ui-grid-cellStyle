96946267db308e720ad7764f65a7e75983e92095
ningfeifei0823 <ningfeifei0823@users.noreply.github.com>
2016-05-26 22:57:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,56 +12,59 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     ]
   };
 
-  $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json')
-    .success(function(data) {
-      $scope.gridOptions.data = [
-                      {
-                          "name": "Ethel Price",
-                          "gender": "female",
-                          "company": "Enersol",
-                           "color":"#0000FF"
-                      },
-                      {
-                          "name": "Claudine Neal",
-                          "gender": "female",
-                          "company": "Sealoud",
-                           "color":"#EE56FF"
-                      },
-                      {
-                          "name": "Beryl Rice",
-                          "gender": "female",
-                          "company": "Velity",
-                           "color":"#EED77F"
-                      },
-                      {
-                          "name": "Wilder Gonzales",
-                          "gender": "male",
-                          "company": "Geekko",
-                           "color":"#E3457F"
-                      },
-                      {
-                          "name": "Georgina Schultz",
-                          "gender": "female",
-                          "company": "Suretech",
-                           "color":"#E2377F"
-                      },
-                      {
-                          "name": "Carroll Buchanan",
-                          "gender": "male",
-                          "company": "Ecosys",
-                           "color":"#99977F"
-                      },
-                      {
-                          "name": "Valarie Atkinson",
-                          "gender": "female",
-                          "company": "Hopeli",
-                           "color":"#00000F"
-                      },
-                      {
-                          "name": "Schroeder Mathews",
-                          "gender": "male",
-                          "company": "Polarium",
-                           "color":"#EED77F"
-                      }];
-    });
+  //$http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json')
+  //  .success(function(data) {
+  //    $scope.gridOptions.data = data;
+  //  });
+    
+  $scope.gridOptions.data = [
+        {
+            "name": "Ethel Price",
+            "gender": "female",
+            "company": "Enersol",
+             "color":"#0000FF"
+        },
+        {
+            "name": "Claudine Neal",
+            "gender": "female",
+            "company": "Sealoud",
+             "color":"#EE56FF"
+        },
+        {
+            "name": "Beryl Rice",
+            "gender": "female",
+            "company": "Velity",
+             "color":"#EED77F"
+        },
+        {
+            "name": "Wilder Gonzales",
+            "gender": "male",
+            "company": "Geekko",
+             "color":"#E3457F"
+        },
+        {
+            "name": "Georgina Schultz",
+            "gender": "female",
+            "company": "Suretech",
+             "color":"#E2377F"
+        },
+        {
+            "name": "Carroll Buchanan",
+            "gender": "male",
+            "company": "Ecosys",
+             "color":"#99977F"
+        },
+        {
+            "name": "Valarie Atkinson",
+            "gender": "female",
+            "company": "Hopeli",
+             "color":"#00000F"
+        },
+        {
+            "name": "Schroeder Mathews",
+            "gender": "male",
+            "company": "Polarium",
+             "color":"#EED77F"
+        }
+  ];
 }]);
